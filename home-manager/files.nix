9f0724f059b01b94ccfe176cf8dfc8f33638216e
Boyan K. <boyan@confest.im>
2026-01-27 01:28:12 +0100

    HM: auto update

--- home-manager/files.nix
+++ home-manager/files.nix
@@ -7,45 +7,26 @@
     xdg-utils
   ];
 
- xdg.mimeApps = {
+ xdg = {
     enable = true;
-    defaultApplications = {
-      # thunar for folders and zips
-      "inode/directory" = "thunar.desktop";
-      "application/zip" = "thunar.desktop";
-      "application/x-tar" = "thunar.desktop";
-      "application/gzip" = "thunar.desktop";
-      
-      "image/png" = "imv.desktop";
-      "image/jpeg" = "imv.desktop";
-      "image/jpg" = "imv.desktop";
-      "image/gif" = "imv.desktop";
-      "image/webp" = "imv.desktop";
-      "image/svg+xml" = "imv.desktop";
-      "image/bmp" = "imv.desktop";
-      "image/tiff" = "imv.desktop";
-      
-      "video/mp4" = "mpv.desktop";
-      "video/x-matroska" = "mpv.desktop";
-      "video/webm" = "mpv.desktop";
-      "video/x-msvideo" = "mpv.desktop";
-      "video/quicktime" = "mpv.desktop";   # .mov
-      "video/x-flv" = "mpv.desktop";
-      "video/mpeg" = "mpv.desktop";
-      "video/ogg" = "mpv.desktop";
-      
-      "audio/mpeg" = "mpv.desktop";        # .mp3
-      "audio/ogg" = "mpv.desktop";
-      "audio/flac" = "mpv.desktop";
-      "audio/x-wav" = "mpv.desktop";
-      "audio/aac" = "mpv.desktop";
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        # thunar for folders and zips
+        "inode/directory" = "thunar.desktop";
+        "application/zip" = "thunar.desktop";
+        "application/x-tar" = "thunar.desktop";
+        "application/gzip" = "thunar.desktop";
+        
+        "image/*" = "imv.desktop";
+        "video/*" = "mpv.desktop";
+        "audio/*" = "mpv.desktop";
 
-      "application/pdf" = "firefox.desktop";
-      # markdown
-      "text/markdown" = "nvim.desktop";
-      "text/*" = "nvim.desktop";
-    };
+        "application/pdf" = "firefox.desktop";
+        "text/*" = "nvim.desktop";
+      };
   };
+ };
   programs.mpv = {
   config = {
     keepaspect-window = true;
