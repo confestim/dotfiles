c816618329cb8b4b010d40e0f2d0be60998c4731
Boyan K. <boyan@confest.im>
2026-05-22 18:42:18 +0200

    HM: auto update

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -6,11 +6,7 @@
 
     completionInit = ''
       autoload -Uz compinit
-      if [[ -n ~/.zcompdump(#qN.mh+24) ]]; then
-        compinit
-      else
-        compinit -C
-      fi
+      compinit -C
     '';
 
     plugins = [
