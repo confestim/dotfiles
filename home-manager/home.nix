43b4ad2567950994b8bcc7e9e85f777c7cbebd71
boyan <boyan@confest.im>
2026-06-21 20:40:29 +0200

    whoops

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,11 +14,9 @@
   xdg.portal = {
     enable = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal-wlr
       pkgs.xdg-desktop-portal-gtk
     ];
     config.common.default = [
-      "wlr"
       "gtk"
     ];
   };
